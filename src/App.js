1e8c0a92bf997858f00b0eca2ed1a4686c7ebb85
SeoKai <ssw123c@gmail.com>
2024-12-12 10:45:50 +0900

    .

--- src/App.js
+++ src/App.js
@@ -1,8 +1,18 @@
-import logo from "./logo.svg";
-import "./App.css";
+import logo from './logo.svg';
+import './App.css';
+
+import Timer from './components/UseEffectExam'
+import ParentComponent_01 from './components/01Parent';
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Timer />
+      <hr/>
+      <ParentComponent_01 />
+
+    </div>
+  );
 }
 
 export default App;
